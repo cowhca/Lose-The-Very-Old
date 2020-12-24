00fecc296fdeb1ce46222b690092150f24050410
cowhca <54647410+cowhca@users.noreply.github.com>
2020-12-24 08:26:56 -0800

    Add files via upload

--- very.js
+++ very.js
@@ -27,17 +27,11 @@ async function callingFn() {
         console.error("Error:", error);
     }
 }
-// function showhide() {   
-//     console.log("helo");          
-//     let form = document.getElementById("add-word");
-//     form.classList.toggle('hidden'); 
-// }
-
 
 (async () => {
     var res = await callingFn();
     // Class attributes for output (doesn't include color)
-    var textClass = "text-center sm:text-3xl md:text-4xl lg:text-5xl font-bold font-serif";
+    // var textClass = "text-center sm:text-3xl md:text-4xl lg:text-5xl font-bold font-serif";
 
     async function fetch_concise_adjective(results, random=false) {
         const output = document.getElementById('output');
@@ -51,7 +45,8 @@ async function callingFn() {
         if (matching_row == undefined) {
             output.innerHTML = "Not Yet Added";
             // Make the text red
-            output.setAttribute("class", textClass + " text-red-400");
+            output.classList.remove("text-green-400");
+            output.classList.add("text-red-400");
         } else {
             // There are multiple options for many of the adjectives
             // Each time we should get a random option
@@ -65,7 +60,8 @@ async function callingFn() {
             
             output.innerHTML = selection; // The fields are labels 'Concise1' 'Concise2' etc...
             // Make the text green
-            output.setAttribute("class", textClass + " text-green-400");
+            output.classList.remove("text-red-400");
+            output.classList.add("text-green-400");
         }
         
     }
@@ -75,7 +71,7 @@ async function callingFn() {
     const picked_word = get_random(res);
     input.setAttribute("placeholder", picked_word.fields.Simple);
     output.innerHTML = picked_word.fields["Concise1"];
-    output.setAttribute("class", "text-center sm:text-3xl md:text-4xl lg:text-5xl font-bold font-serif text-gray-500");
+    output.classList.add("text-gray-500");
     
 
     // Get a random word pair when the button is pressed
@@ -84,12 +80,6 @@ async function callingFn() {
          fetch_concise_adjective(res, true);
       });
     });
-
-    // $(document).ready(function(){
-    //     $('#add-word').click(function(){
-    //         showhide();
-    //     });
-    // })
 
     // Fetch result when 'Get Result' button is pressed
     $(document).ready(function(){
